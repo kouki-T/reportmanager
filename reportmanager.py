68d929c5ac70da7e60fc337eaf82b3f4ba527562
kouki-T <badapple444@gmail.com>
2019-06-02 17:37:38 +0900

    複数セルに対応

--- reportmanager.py
+++ reportmanager.py
@@ -18,5 +18,28 @@ req = requests.get(url=url, headers=headers)
 
 soup = BeautifulSoup(req.content, "xml")
 
-wks.update_acell('F14', str(soup.item.link.string))
-print(wks.acell('F14'))
+def alpha2num(alpha):
+    num=0
+    for index, item in enumerate(list(alpha)):
+        num += pow(26,len(alpha)-index-1)*(ord(item)-ord('A')+1)
+    return num
+
+def num2alpha(num):
+    if num<=26:
+        return chr(64+num)
+    elif num%26==0:
+        return num2alpha(num//26-1)+chr(90)
+    else:
+        return num2alpha(num//26)+chr(64+num%26)
+
+cell = alpha2num("AA")
+
+check_data = wks.acell( num2alpha(cell) + "14").value
+
+while "http" in check_data:
+    cell = cell + 1
+    check_data = wks.acell( num2alpha(cell) + "14").value
+else:
+    wks.update_acell( num2alpha(cell) + "14", str(soup.item.link.string))
+
+print(num2alpha(cell) + "14" + "is compleate.")
